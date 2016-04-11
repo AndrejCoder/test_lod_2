8f5dd2923a46e166a57df6c13f9e3a22209d4793
a_belov <abelov@lanit.ru>
2016-04-11 17:54:42 +0300

    Форматирование кода.

--- reqs/api.py
+++ reqs/api.py
@@ -1,7 +1,6 @@
 # coding: utf-8
-from rest_framework import filters
 from rest_framework.decorators import detail_route
-from rest_framework.filters import DjangoFilterBackend
+from rest_framework.filters import DjangoFilterBackend, OrderingFilter, SearchFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -14,31 +13,14 @@ from reqs.models import Request, ActivityPlace
 from reqs.serializers import RequestSerializer, ActivityPlaceSerializer
 
 
-class JqGridRequestViewset(JqGridModelViewSet):
-
-    no_json_fields = ['id', ]
-
-    serializer_class = RequestSerializer
-    filter_backends = (filters.SearchFilter, JqGridDjangoFilterBackend, filters.OrderingFilter)
-    search_fields = ('id', 'json_data')
-    # ordering_fields = ('last_name', 'first_name', 'second_name', 'birthday', 'place', 'age')
-    filter_class = RequestFilter
-    queryset = Request.objects.all()
-
-    pagination_class = JqGridPageNumberPagination
-    pagination_class.page_size = 20
-    pagination_class.page_size_query_param = 'rows'
-
-
 class RequestViewset(ModelViewSet):
 
     no_json_fields = ['id', ]
     json_field = 'json_data'
 
     serializer_class = RequestSerializer
-    filter_backends = (filters.SearchFilter, DjangoFilterBackend, filters.OrderingFilter)
+    filter_backends = (SearchFilter, DjangoFilterBackend, OrderingFilter)
     search_fields = ('id', 'json_data')
-    # ordering_fields = ('last_name', 'first_name', 'second_name', 'birthday', 'place', 'age')
     filter_class = RequestFilter
     queryset = Request.objects.all()
 
@@ -61,10 +43,24 @@ class RequestViewset(ModelViewSet):
         return Response(serializer.data)
 
 
+class JqGridRequestViewset(JqGridModelViewSet):
+    no_json_fields = ['id', ]
+
+    serializer_class = RequestSerializer
+    filter_backends = (SearchFilter, JqGridDjangoFilterBackend, OrderingFilter)
+    search_fields = ('id', 'json_data')
+    filter_class = RequestFilter
+    queryset = Request.objects.all()
+
+    pagination_class = JqGridPageNumberPagination
+    pagination_class.page_size = 20
+    pagination_class.page_size_query_param = 'rows'
+
+
 class ActivityPlaceViewset(JqGridModelViewSet):
     no_json_fields = ['id', 'request']
     serializer_class = ActivityPlaceSerializer
-    filter_backends = (filters.SearchFilter, JqGridDjangoFilterBackend, filters.OrderingFilter)
+    filter_backends = (SearchFilter, JqGridDjangoFilterBackend, OrderingFilter)
     search_fields = ('id', 'json_data')
     # ordering_fields = ('last_name', 'first_name', 'second_name', 'birthday', 'place', 'age')
     filter_class = ActivityPlaceFilter
--- reqs/urls.py
+++ reqs/urls.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from rest_framework.routers import SimpleRouter
 
 from reqs.api import RequestViewset, JqGridRequestViewset
 
 router = SimpleRouter()
 router.register(r'request', RequestViewset, 'request')
-router.register(r'jqgrid-request', JqGridRequestViewset, 'jqgrid-request')
+if 'jqgrid_django_rest_framework' in settings.INSTALLED_APPS:
+    router.register(r'jqgrid-request', JqGridRequestViewset, 'jqgrid-request')
 # router.register(r'activity-place', ActivityPlaceViewset, 'activity-place')
 
 urlpatterns = router.urls
